451ac580c7ec68a9a6a14ffd21d30e26f133af32
iagonzal97 <iagonzal97@gmail.com>
2021-09-19 23:03:11 -0400

    added main method and prompt for user input

--- guesser.py
+++ guesser.py
@@ -1,10 +1,13 @@
 #Source: https://www.youtube.com/watch?v=EBezqslQslc
-
 from random import randrange
-n = randrange(1000)
-while True:
-    v = int(input())
-    if n == v: #if it is the correct number
-        print("You win!")
-        break
-    print("Smaller" if (n < v) else "Bigger")
+def guess():
+    n = randrange(1000)
+    while True:
+        v = int(input("Please enter your guess for the correct number: "))
+        if n == v: #if it is the correct number
+            print("You win!")
+            break
+        print("Smaller" if (n < v) else "Bigger")
+
+if __name__ == '__main__':
+    guess()
